cfe2b00e9e5758873a96bd21f5c54891a98eec22
“Amila <akudahewa@gmail.com>
2020-03-05 14:04:24 +0530

    Sqush commits 4 to 7

--- rebase_test.rb
+++ rebase_test.rb
@@ -1,3 +1,7 @@
 First commit for rebase_branch
 Second commit for rebase branch
 Third commit for rebase branch
+fourth commit for rebase branch  ### after rebase commit start from here
+Fiveth commit for rebase branch
+Sixth commit for rebase branch
+Seventh commit for rebase branch
